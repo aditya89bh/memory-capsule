041183c97967ad0e4d227faac5601fccc2ea52da
Aditya bhatia <aditya89bh@gmail.com>
2026-05-25 20:40:33 +0530

    Export merge utilities from package

--- capsule/__init__.py
+++ capsule/__init__.py
@@ -1,7 +1,15 @@
 """Portable memory capsules for AI agents."""
 
+from .merge import MergeReport, merge_capsules
 from .schema import MemoryCapsule, MemoryItem
 from .store import CapsuleStore
 from .init import new_capsule
 
-__all__ = ["MemoryCapsule", "MemoryItem", "CapsuleStore", "new_capsule"]
+__all__ = [
+    "MemoryCapsule",
+    "MemoryItem",
+    "CapsuleStore",
+    "MergeReport",
+    "merge_capsules",
+    "new_capsule",
+]
